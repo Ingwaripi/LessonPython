d7fa9102e0e0ae18951588285d8ac057aee698ef
Ekaterina Nikolaeva <ekaterina266226@mail.ru>
2022-10-11 16:32:48 +0400

    Add home work 123

---
+++ 4_Lesson/HomeWork4/homwor2.py
@@ -0,0 +1,21 @@
+# 2. Задайте натуральное число N. Напишите программу,
+#  которая составит список простых множителей числа N.
+# Простые делители числа
+# in
+# 54
+# out
+# [2, 3, 3, 3]
+
+
+def find_prime_nums(num):
+    pr_fact = []
+    di = 2
+    while num > 1:
+        if num % di == 0: 
+            pr_fact.append(di)
+            num //= di
+        else:
+            di +=1
+    return pr_fact
+
+print(find_prime_nums(int(input())))
